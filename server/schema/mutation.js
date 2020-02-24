6d43edeec4c57c13f678afcaf2f15f31196f8f4f
radu pacurar <radu.pacurar@hitta.se>
2020-02-24 15:49:54 +0200

    with login

--- server/schema/mutation.js
+++ server/schema/mutation.js
@@ -21,21 +21,47 @@ const mutation = new GraphQLObjectType({
                 email: { type: new GraphQLNonNull(GraphQLString) },
                 password: { type: new GraphQLNonNull(GraphQLString) },
             },
-            async resolve(parentValue, { name, email, password }) {
+            async resolve(parentValue, { name, email, password: rawPassword }) {
 
-                const encryptPass = await bcrypt.hash(password, 10);
+                const password = await bcrypt.hash(rawPassword, 10);
 
                 const user = await axios.post(
                     'http://localhost:3300/users', 
-                    { name, email, encryptPass }
+                    { name, email, password }
                 ).then(res => res.data);
 
                 const token = jwt.sign({ userId: user.id }, APP_SECRET);
 
-                console.log(token, APP_SECRET)
+                return { 
+                    ...user, 
+                    token
+                };
 
-                return { ...user, token };
+            }
+        },
+        signIn: {
+            type: UserType,
+            args: {
+                email: { type: new GraphQLNonNull(GraphQLString) },
+                password: { type: new GraphQLNonNull(GraphQLString) },
+            },
+            async resolve(parentValue, { email, password: checkPassword }) {
+                const user = await axios.get(
+                    `http://localhost:3300/users?email=${email}`
+                ).then(res => res.data[0]);
 
+                const matched = await bcrypt.compare(checkPassword, user.password);
+                if (!matched) {
+                    throw new Error('invalid password');
+                }
+
+                const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+                return { 
+                    ...user, 
+                    token
+                };
+                
             }
         }
     }
